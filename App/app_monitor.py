948477fff9b671af6e6344df50b409d308e73989
GounGG <913956964@qq.com>
2017-11-24 10:44:59 +0800

    忽略已经消亡的进程

--- App/app_monitor.py
+++ App/app_monitor.py
@@ -1,42 +1,59 @@
 #!/usr/bin/env python
-
-import psutil
+# coding:utf-8
+from psutil import pids,Process
 import re
 import json
 import sys
 
-def Process(name):
+def F_Process(pname):
     result = []
-    try:
-        a = psutil.pids()
-        for i in a:
-            p = psutil.Process(i)
+    a = pids()
+    for i in a:
+	try:
+            p = Process(i)
             if p.name() == 'java':
 	        cmd = ' '.join(p.cmdline())
-		if re.search(name, cmd):
+		if re.search(pname, cmd):
 		    result.append(0)
 		else:
 		    result.append(1)
-    except Exception,e:
-        print 1
+    	except Exception,e:
+            continue
+    if 0 in result and result.count(0) == 1:
+        print 0
     else:
-	if 0 in result and result.count(0) == 1:
-	    print 0
-	else:
-	    print 1
+        print 1
 
-def Memory(name):
-    a = psutil.pids()
+def F_Memory(pname):
+    a = pids()
     for i in a:
-        p = psutil.Process(i)
-        if p.name() == 'java':
-            cmd = ' '.join(p.cmdline())
-            if re.search(name, cmd):
-	        print round(p.memory_percent(), 2)
+	try:
+            p = Process(i)
+            if p.name() == 'java':
+                cmd = ' '.join(p.cmdline())
+                if re.search(pname, cmd):
+	            print round(p.memory_percent(), 2)
+    	except Exception,e:
+	    continue
 
-name = sys.argv[1]
-param = sys.argv[2] 
-if name == 'Process':
-    Process(param)
-elif name == 'Memory':
-    Memory(param)
+def F_Threads(pname):
+    a = pids()
+    for i in a:
+	try:
+            p = Process(i)
+            if p.name() == 'java':
+            	cmd = ' '.join(p.cmdline())
+            	if re.search(pname, cmd):
+	            print p.num_threads()
+    	except Exception,e:
+	    continue
+
+pname = sys.argv[1]
+param = sys.argv[2]
+ 
+if pname == 'Process':
+    F_Process(param)
+elif pname == 'Memory':
+    F_Memory(param)
+elif pname == "Threads":
+    F_Threads(param)
